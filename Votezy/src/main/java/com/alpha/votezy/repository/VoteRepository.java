35ad17429175e32fa55f9ba61ff42bcf207ad55d
rohithvoddam0606 <voddamrohith@gmail.com>
2026-05-12 17:25:27 +0530

    Removed nested Votezy folder

--- Votezy/src/main/java/com/alpha/votezy/repository/VoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alpha.votezy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.alpha.votezy.entity.Vote;
-
-public interface VoteRepository extends JpaRepository<Vote, Long>{
-
-}
--- Votezy/src/main/java/com/alpha/votezy/repository/VoterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alpha.votezy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.alpha.votezy.entity.Voter;
-
-public interface VoterRepository extends JpaRepository<Voter, Long>{
-	boolean existsByEmail(String email);
-}
